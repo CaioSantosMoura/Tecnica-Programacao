9da473f0925cb804a8004ecc47e235a8f5e11d95
Caio <caio_s_moura@estudante.sesisenai.org.br>
2024-08-01 18:46:50 -0300

    .

--- Aula01-08.c
+++ Aula01-08.c
@@ -1,3 +1,26 @@
+// --- 1) Quanto de memória a variável arquivo irá utilizar durante a execução do programa? ---
+// Variável arquivo: O consumo de memória depende do tipo de dado. Para um ponteiro FILE, geralmente ocupa 8 bytes em sistemas de 64 bits.
+
+// --- 2) Um ponteiro FILE em linguagem C pode armazenar quantos bytes? ---
+// Ponteiro FILE: Em sistemas de 64 bits, ocupa 8 bytes. Em sistemas de 32 bits, ocupa 4 bytes.
+
+// 3) Um ponteiro int pode armazenar quantos bytes?
+// Ponteiro int: Em sistemas de 64 bits, ocupa 8 bytes. Em sistemas de 32 bits, ocupa 4 bytes.
+
+// 4) Analise os dois programas a seguir:
+// Programa 1:
+int main ()
+{
+   float matrx [50][50];
+   int i,j;
+   for (i=0;i<50;i++)
+       for (j=0;j<50;j++)
+           matrx[i][j]=0.0;
+   return(0);
+}
+
+
+// Programa 2:
 int main ()
 {
    float matrx [50][50];
@@ -10,3 +33,8 @@ int main ()
    }
    return(0);
 }
+
+// Quanta memória cada programa ocupa durante a sua execução?
+// Memória dos programas:
+    //  Programa 1: Usa 50 × 50 × 4 bytes = 10.000 bytes (4 bytes por float).
+    //  Programa 2: Usa a mesma quantidade, 50 × 50 × 4 bytes = 10.000 bytes.
