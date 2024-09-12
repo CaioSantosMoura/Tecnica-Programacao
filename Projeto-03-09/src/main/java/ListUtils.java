5f19a8c221bf5307f712e055629c88bc7d42d66c
Caio <caio_s_moura@estudante.sesisenai.org.br>
2024-09-12 18:00:43 -0300

    .

---
+++ Projeto-03-09/src/main/java/ListUtils.java
@@ -0,0 +1,15 @@
+import java.util.List;
+public class ListUtils {
+
+    public ListUtils() {
+    }
+    public void addToList(List<String> list, String item){
+        list.add(item);
+    }
+    public void removeFromList(List<String> list, String item){
+        list.remove(item);
+    }
+    public boolean findInList(List<String> list, String item){
+        return list.contains(item);
+    }
+}
